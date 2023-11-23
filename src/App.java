7a5f06d606df4a396ad1b31daa6c6ed4de5bc2e0
Vitoria <mvitoriaberto2@gmail.com>
2023-11-23 15:57:31 -0300

    quickstart

---
+++ src/App.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner (System.in);
+
+        imprimirTraco();
+
+        System.out.println("Insira o tamanho do array desejado: ");
+        int tam = sc.nextInt();
+        int[] arr = new int[tam];
+        
+
+        
+    }
+
+    public static void imprimirTraco() {
+        System.out.println("-------------------------------------");
+   }
+}
