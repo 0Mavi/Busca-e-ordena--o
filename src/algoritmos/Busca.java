83804f4e87e1fd55e2bf35108e859cb3df0eea2e
luizfiliperm <luizfilipemr17@gmail.com>
2023-11-23 17:57:00 -0300

    Comentando os algorítmos

--- src/algoritmos/Busca.java
+++ src/algoritmos/Busca.java
@@ -3,6 +3,15 @@ package algoritmos;
 public class Busca {
 
     public static void buscaLinear (int[] arr, int num){
+
+        /*
+            1. Percorra a array
+            2. Compare o elemento atual com o elemento buscado
+            3. Se o elemento atual for igual ao elemento buscado, retorne a posição do elemento atual
+            4. Se o elemento atual for diferente do elemento buscado, continue a percorrer a array
+            5. Se o elemento buscado não for encontrado, retorne uma mensagem de erro
+         */
+
         int tam = arr.length;
 
         for(int i = 0; i < tam; i++){
@@ -16,28 +25,42 @@ public class Busca {
     }
 
     public static void buscaBinaria(int[] arr, int num){
+
+        /*
+            1. Defina o inicio, meio e fim da array
+            2. Compare o elemento do meio com o elemento buscado
+            3. Se o elemento do meio for igual ao elemento buscado, retorne a posição do elemento do meio
+            4. Se o elemento do meio for menor que o elemento buscado, defina o inicio como o elemento do meio + 1
+            5. Se o elemento do meio for maior que o elemento buscado, defina o fim como o elemento do meio - 1
+            6. Se o inicio for maior que o fim, retorne uma mensagem de erro
+         */
+
         int tam = arr.length;
 
         int inicio = 0;         
         int meio = 0;         
         int fim = tam - 1;
 
-
+        // Enquanto o inicio for menor ou igual ao fim
         while (inicio <= fim) {
+            // Define o meio como a metade da array
             meio = (fim + inicio) / 2;
 
+            // Se o elemento do meio for igual ao elemento buscado, retorne a posição do elemento do meio
             if(arr[meio] == num) {
                 System.out.println("Encontrou o número: " + num + " na posição: " + meio);
                 
                 break;
             }
 
+            // Se o elemento do meio for menor que o elemento buscado, defina o inicio como o elemento do meio + 1
             if(arr[meio] < num) {
                 inicio = meio + 1;
               } else {
                 fim = meio - 1;
             }
 
+            // Se o inicio for maior que o fim, retorne uma mensagem de erro
             if(inicio > fim) {
                 System.out.println("Não encontrou o número: " + num);
             }
--- src/algoritmos/Ordenacao.java
+++ src/algoritmos/Ordenacao.java
@@ -3,12 +3,24 @@ package algoritmos;
 public class Ordenacao {
     
     public static void bubbleSort(int[] arr){
+        /*
+           1. Compara o elemento atual com o próximo elemento.
+           2. Se o elemento atual for maior que o próximo elemento, troque-os.
+           3. Se o elemento atual for menor que o próximo elemento, não faça nada.
+           4. Repita o passo 1 até o final do array.
+           5. Repita os passos 1, 2, 3 e 4 até que não haja mais trocas.
+        */
+
         int tam = arr.length;
         int temp = 0;
 
+        // Percorre a array
         for(int i = 0; i < tam; i++){
+            // Percorre a array a partir do elemento atual
             for(int j = 1; j < (tam - i); j++){
+                // Compara o elemento atual com o próximo elemento
                 if (arr[j-1] > arr[j]){
+                    // Troca os elementos se o elemento atual for maior que o próximo elemento
                     temp = arr[j-1];
                     arr[j-1] = arr[j];
                     arr[j] = temp;
@@ -18,14 +30,25 @@ public class Ordenacao {
     }
 
     public static void selectionSort(int[] arr){
+        /*
+          1. Encontre o menor elemento da array.
+          2. Troque o menor elemento com o primeiro elemento da array.
+          3. Repita o passo 1 e 2 até que a array esteja ordenada.
+         */
+
         int tam = arr.length;
         int temp = 0;
         int min = 0;
 
+        // Percorre a array
         for(int i = 0; i < tam; i++){
+            // Define o primeiro elemento como o menor
             min = i;
+            // Percorre a array a partir do elemento atual
             for(int j = i; j < tam ; j++){
+                // Compara o elemento atual com o menor
                 if(arr[j] < arr[min]){
+                    // Se o elemento atual for menor que o menor, define o elemento atual como o menor
                     min = j;
                 }
             }
@@ -36,11 +59,22 @@ public class Ordenacao {
    }
 
    public static void insertionSort(int[] arr){
+        /*
+            1. Percorra a array a partir do segundo elemento.
+            2. Compare o elemento atual com seu antecessor.
+            3. Se o elemento atual for menor que seu antecessor, troque-os.
+            4. Repita o passo 2 e 3 até que o elemento atual seja maior que seu antecessor.
+            5. Repita os passos 1, 2, 3 e 4 até que a array esteja ordenada.
+         */
+
         int tam = arr.length;
         int temp = 0;
 
+        // Percorre a array a partir do segundo elemento
         for(int i = 1; i < tam; i++){
+            // define o elemento atual como o elemento a ser comparado
             int x = i;
+            // Compara o elemento atual com seu antecessor até que o elemento atual seja maior que seu antecessor
             while (x > 0 && arr[x-1] > arr[x]) {
                 temp = arr[x];
                 arr[x] = arr[x-1];
